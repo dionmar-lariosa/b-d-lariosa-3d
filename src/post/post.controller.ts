507464080a0c7412234b0ba4d99fa1811d457186
dionmar-lariosa <main.dionmar@gmail.com>
2024-08-29 19:55:15 +0800

    crud

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,5 +1,16 @@
-import { Controller, Get, InternalServerErrorException } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  InternalServerErrorException,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { PostService } from './post.service';
+import { Prisma } from '@prisma/client';
 
 @Controller('post')
 export class PostController {
@@ -9,6 +20,45 @@ export class PostController {
   async getAll() {
     try {
       return await this.postService.getAll();
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  @Get(':id')
+  async get(@Param('id', ParseIntPipe) id: number) {
+    try {
+      return await this.postService.get(id);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  @Post()
+  async create(@Body() dto: Prisma.PostCreateInput) {
+    try {
+      return await this.postService.create(dto);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  @Patch(':id')
+  async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() dto: Prisma.PostUpdateInput,
+  ) {
+    try {
+      return await this.postService.update(id, dto);
+    } catch (error) {
+      throw new InternalServerErrorException(error.message);
+    }
+  }
+
+  @Delete(':id')
+  async delete(@Param('id', ParseIntPipe) id: number) {
+    try {
+      return await this.postService.delete(id);
     } catch (error) {
       throw new InternalServerErrorException(error.message);
     }
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
@@ -7,5 +8,26 @@ export class PostService {
 
   async getAll() {
     return await this.prismaService.post.findMany();
+  }
+
+  async get(id: number) {
+    return await this.prismaService.post.findUniqueOrThrow({
+      where: { id: id },
+    });
+  }
+
+  async create(dto: Prisma.PostCreateInput) {
+    return await this.prismaService.post.create({ data: dto });
+  }
+
+  async update(id: number, dto: Prisma.PostUpdateInput) {
+    return await this.prismaService.post.update({
+      where: { id: id },
+      data: dto,
+    });
+  }
+
+  async delete(id: number) {
+    return await this.prismaService.post.delete({ where: { id: id } });
   }
 }
